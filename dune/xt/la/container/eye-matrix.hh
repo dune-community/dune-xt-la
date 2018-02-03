5bde5f7826248db7e6db8f19209f67c891e568e6
Tobias Leibner <tobias.leibner@uni-muenster.de>
2018-02-03 02:14:22 +0100

    [container.eye-matrix] add method to create on heap

--- dune/xt/la/container/eye-matrix.hh
+++ dune/xt/la/container/eye-matrix.hh
@@ -17,30 +17,48 @@
 namespace Dune {
 namespace XT {
 namespace LA {
+namespace internal {
+
+
+SparsityPatternDefault create_eye_pattern(const size_t rows, const size_t cols)
+{
+  SparsityPatternDefault pattern(rows);
+  for (size_t ii = 0; ii < std::min(rows, cols); ++ii)
+    pattern.insert(ii, ii);
+  // each row has to contain at least one non-zero entry
+  for (size_t ii = std::min(rows, cols); ii < std::max(rows, cols); ++ii)
+    pattern.insert(ii, 0);
+  return pattern;
+}
+
+template <class M>
+typename std::enable_if<is_matrix<M>::value, void>::type set_diagonal_to_one(M& mat)
+{
+  for (size_t ii = 0; ii < std::min(mat.rows(), mat.cols()); ++ii)
+    mat.set_entry(ii, ii, 1.);
+}
+
+
+} // namespace internal
 
 
 template <class M>
 typename std::enable_if<is_matrix<M>::value, M>::type eye_matrix(const size_t rows, const size_t cols)
 {
-  if (M::sparse) {
-    SparsityPatternDefault pattern(rows);
-    for (size_t ii = 0; ii < std::min(rows, cols); ++ii)
-      pattern.insert(ii, ii);
-    // each row has to contain at least one non-zero entry
-    for (size_t ii = std::min(rows, cols); ii < std::max(rows, cols); ++ii)
-      pattern.insert(ii, 0);
-    M mat(rows, cols, pattern);
-    for (size_t ii = 0; ii < std::min(rows, cols); ++ii)
-      mat.set_entry(ii, ii, 1);
-    return mat;
-  } else {
-    M mat(rows, cols, 0.);
-    for (size_t ii = 0; ii < std::min(rows, cols); ++ii)
-      mat.set_entry(ii, ii, 1);
-    return mat;
-  }
+  M mat = M::sparse ? M(rows, cols, internal::create_eye_pattern(rows, cols)) : M(rows, cols, 0.);
+  internal::set_diagonal_to_one(mat);
+  return mat;
 }
 
+template <class M>
+typename std::enable_if<is_matrix<M>::value, std::unique_ptr<M>>::type eye_matrix_ptr(const size_t rows,
+                                                                                      const size_t cols)
+{
+  auto mat = M::sparse ? std::make_unique<M>(rows, cols, internal::create_eye_pattern(rows, cols))
+                       : std::make_unique<M>(rows, cols, 0.);
+  internal::set_diagonal_to_one(*mat);
+  return mat;
+}
 
 template <class M>
 typename std::enable_if<Common::is_matrix<M>::value && !is_matrix<M>::value, M>::type eye_matrix(const size_t rows,
@@ -53,11 +71,27 @@ typename std::enable_if<Common::is_matrix<M>::value && !is_matrix<M>::value, M>:
   return mat;
 }
 
+template <class M>
+typename std::enable_if<Common::is_matrix<M>::value && !is_matrix<M>::value, std::unique_ptr<M>>::type
+eye_matrix_ptr(const size_t rows, const size_t cols)
+{
+  using Abstraction = Common::MatrixAbstraction<M>;
+  auto mat = Abstraction::create_dynamic(rows, cols, 0.);
+  for (size_t ii = 0; ii < std::min(rows, cols); ++ii)
+    Abstraction::set_entry(*mat, ii, ii, 1);
+  return mat;
+}
 
 template <class M>
 typename std::enable_if<Common::is_matrix<M>::value, M>::type eye_matrix(const size_t size)
 {
   return eye_matrix<M>(size, size);
+}
+
+template <class M>
+typename std::enable_if<Common::is_matrix<M>::value, std::unique_ptr<M>>::type eye_matrix_ptr(const size_t size)
+{
+  return eye_matrix_ptr<M>(size, size);
 }
 
 
